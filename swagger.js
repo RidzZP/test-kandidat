efead56ca3dda4885c9c167893da3c8d7002412d
RidzZP <ridzprog@gmail.com>
2026-01-12 13:31:23 +0700

    update development server URL to use HTTPS

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000",
+                url: "https://test-kandidat.eurekagroup.id",
                 description: "Development server",
             },
         ],
